e6331474cdacc502fde46eee8f602881e7cc1eb0
vignesh5oct <vignesh5oct@gmail.com>
2024-04-30 12:24:41 +0530

    after login/registerpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Body from './Components/Body';
+import Header from './Components/Header';
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Header/>
+      <Body/>
     </div>
   );
 }
---
+++ src/Components/Body.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Login from './Login'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Browse from './Browse'
+
+const Body = () => {
+
+
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: "/browse",
+      element: <Browse />
+    },
+  ])
+
+
+  return (
+    <div>
+      <RouterProvider router={appRouter}>
+
+ 
+        <Login />
+
+
+      </RouterProvider>
+    </div>
+  )
+}
+
+export default Body
